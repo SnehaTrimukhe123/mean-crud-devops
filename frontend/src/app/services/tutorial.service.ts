4c2c6fa4e33bbb1566c6db9f6e0cf5e59313f6b4
SnehaTrimukhe123 <snehatrimukhe279@gmail.com>
2026-02-24 14:14:57 +0530

    Fix backend URL for production

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+private baseUrl = 'http://13.63.161.12:5000/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
